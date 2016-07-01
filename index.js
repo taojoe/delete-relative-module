37aa890a24d23a6e62e4b651415623bff0980aa5
joe <taojoe@gmail.com>
2016-07-01 09:59:35 +0800

    调整需要删除的代码

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 
 var del = require('del');
 
-var data=require('./package.json');
-var dependencies=data.dependencies;
+var fs=require('fs');
+
+var package=fs.readFileSync('package.json', "utf8");
+var dependencies=JSON.parse(package).dependencies;
 var to_del=[];
 for(var i in dependencies){
     var version=dependencies[i];
     if(version.startsWith('../')){
-        to_del.push(i);
+        to_del.push('node_modules/'+i);
     }
 }
-
 if(to_del.length>0){
     del(to_del).then(function(paths){
         console.log('Deleted files and folders:\n', paths.join('\n'));
+    }).catch(function(e){
+        console.log(e);
     });
 }
